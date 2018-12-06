d5b5292921f35b33f87266daa4f2ff474f1068cf
WangChen0902 <827913668@qq.com>
2018-12-07 03:33:19 +0800

    first version

---
+++ predict.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import os
+import xlwt
+import model
+
+
+def get_one_image(img_dir):
+    image = Image.open(img_dir)
+    image = image.resize([32, 32])
+    image = image.convert('RGB')
+    image_arr = np.array(image)
+    return image_arr
+
+
+def test(test_file):
+    log_dir = 'saves'
+    image_arr = get_one_image(test_file)
+
+    with tf.Graph().as_default():
+        image = tf.cast(image_arr, tf.float32)
+        image = tf.image.per_image_standardization(image)
+        image = tf.reshape(image, [1, 32, 32, 3])
+        print(image.shape)
+        p = model.mmodel(image, 1)
+        logits = tf.nn.softmax(p)
+        x = tf.placeholder(tf.float32, shape=[32, 32, 3])
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            ckpt = tf.train.get_checkpoint_state(log_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print('Loading success')
+                prediction = sess.run(logits, feed_dict={x: image_arr})
+                max_index = np.argmax(prediction)
+                print(max_index)
+                return max_index
+            else:
+                print('No checkpoint')
+
+
+DATA = []
+i = 0
+dir = 'D:/WCsPy/data/test/'
+workbook = xlwt.Workbook(encoding='utf-8')
+booksheet = workbook.add_sheet('Sheet 1', cell_overwrite_ok=True)
+for file_name in os.listdir(dir):
+    i = i+1
+    #if i > 200:
+    #    break
+    print('Number:', i)
+    label = str(test(dir+file_name))
+    result = [file_name, label]
+    DATA.append(result)
+    print(result)
+
+i = 0
+for row in DATA:
+    j = 0
+    for col in row:
+        booksheet.write(i, j, col)
+        j = j + 1
+    i = i + 1
+workbook.save('result3.xlsx')
---
+++ train.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf
+import os
+import model
+import inputData
+
+
+def run_training():
+    data_dir = 'D:/WCsPy/data/train/'
+    log_dir = 'saves'
+    image, label = inputData.get_files(data_dir)
+    image_batches, label_batches = inputData.get_batches(image, label, 128, 128, 16, 20)
+    print(image_batches.shape)
+    p = model.mmodel(image_batches, 16)
+    cost = model.loss(p, label_batches)
+    train_op = model.training(cost, 0.001)
+    acc = model.get_accuracy(p, label_batches)
+
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    saver = tf.train.Saver()
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    try:
+        for step in np.arange(1000):
+            print(step)
+            if coord.should_stop():
+                break
+            _, train_acc, train_loss = sess.run([train_op, acc, cost])
+            print("loss:{} accuracy:{}".format(train_loss, train_acc))
+            if step % 100 == 0:
+                check = os.path.join(log_dir, "model.ckpt")
+                saver.save(sess, check, global_step=step)
+    except tf.errors.OutOfRangeError:
+        print("Done!!!")
+    finally:
+        coord.request_stop()
+    coord.join(threads)
+    sess.close()
+
+
+run_training()
